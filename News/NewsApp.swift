2890f773f2f33f2d06ec7299820a5dd303c394c9
Jack Finnis <finnis.j@etoncollege.org.uk>
2021-08-03 15:11:19 +0100

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by William Finnis on 03/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
